bdad813ac4155a835d2d52b2587189e455692bef
Kabir Hossain <kabirdkng@gmail.com>
2024-06-22 12:31:53 +0530

    fix

--- vite.config.js
+++ vite.config.js
@@ -4,16 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/random-quran-ayat/',
-  build: {
-    outDir: 'dist',
-    emptyOutDir: true,
-    rollupOptions: {
-      input: 'index.html',
-      output: {
-        dir: 'dist',
-        entryFileNames: 'index.html',
-      },
-    },
-  },
 })
